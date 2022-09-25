a11286f095f43b58efea034cdefe9061ffc1fc48
Minh Ngo <nvminh023@gmail.com>
2022-09-25 21:59:48 +0700

    chore: only lint staged files

--- .githooks/pre-commit/lint-fix.sh
+++ .githooks/pre-commit/lint-fix.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
+
+CHANGED_FILES=$(git diff --cached --name-only --diff-filter=ACMR "*.js" "*.jsx" "*.ts" "*.tsx" | sed 's| |\\ |g')
+
+[[ -z "$CHANGED_FILES" ]] && exit 0
+
 echo "Fixing eslint 🔧 🪚 🔨"
-npm run code:lint-fix
+
+if echo "$CHANGED_FILES" | xargs ./node_modules/.bin/eslint --fix ; then
+    echo "$CHANGED_FILES" | xargs git add
+    echo "Done fixing eslint ✨✨"
+    exit 0
+else
+    echo "Failed fixing eslint, please check the output 🐞"
+    exit 1
+fi
+
